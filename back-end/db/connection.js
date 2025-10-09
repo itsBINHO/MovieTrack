78469cb4f021f749c82d53fc39200b810a19d31f
Robson Rocha <robson.rs0209@gmail.com>
2025-10-09 07:46:15 -0300

    :sparkles: Feat - Create database connection for backend project

---
+++ back-end/db/connection.js
@@ -0,0 +1,20 @@
+import mysql from "mysql2"
+import dotenv from "dotenv"
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to the MySql");
+});
+
+export default db;
