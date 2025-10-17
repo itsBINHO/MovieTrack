cd42d049651b90e4f05e1a309e1b4e8d1d1d0da2
Robson Rocha <robson.rs0209@gmail.com>
2025-10-17 18:48:58 -0300

    :sparkles: Feat - Add Middleware token verification

---
+++ back-end/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+import JsonWebTokenError from "jsonwebtoken";
+
+const SECRET = "";
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Acesso negado!" });
+  }
+
+  JsonWebTokenError.verify(token, SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Token inválido!" });
+    }
+    
+    req.user = user; // Armazena as infos do usuário dentro do req
+    next(); // deixa a requisição seguir para a rota protegida
+  });
+};
