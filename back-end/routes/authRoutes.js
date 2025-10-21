d705476a59363503a6ac405e070a184fada2fc66
Robson Rocha <robson.rs0209@gmail.com>
2025-10-20 23:30:27 -0300

    :recycle: Refactor - Recycle code and added verify token

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+router.get("/dashboard", verifyToken, (req, res) => {
+  res.json({
+    message: `Bem-vindo ao dashboard, ${req.user.email}!`,
+  });
+});
 
 export default router;
