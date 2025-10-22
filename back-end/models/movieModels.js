02aeae7b10ec8c82ad55e13cf73534129cdb91cc
Robson Rocha <robson.rs0209@gmail.com>
2025-10-22 19:11:57 -0300

    :sparkles: Feat - Create CRUD

--- back-end/models/movieModels.js
+++ back-end/models/movieModels.js
@@ -8,3 +8,25 @@ export const createMovie = async (movie) => {
   );
   return result;
 };
+
+export const getMoviesByUserId = async (user_id) => {
+  const [rows] = await db.query(
+    "SELECT * FROM movies WHERE user_id = ?",
+    [user_id]
+  );
+  return rows;
+};
+
+export const updateMovie = async (id, movie, user_id) => {
+  const { title, genre, type, release_year, description } = movie;
+  const [result] = await db.query(
+    "UPDATE movies SET title = ?, genre = ?, type = ?, release_year = ?, description = ? WHERE id = ? AND user_id = ?",
+    [title, genre, type, release_year, description, id, user_id]
+  );
+  return result;
+};
+
+export const deleteMovie = async (id, user_id) => {
+  const [result] = await db.query("DELETE FROM movies WHERE id = ? AND user_id = ?", [id, user_id]);
+  return result;
+};
